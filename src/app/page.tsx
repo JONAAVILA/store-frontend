913d343366bae513343cca1ca5111419d70663b6
sidonay499 <jona_1969@hotmail.com>
2025-07-25 12:38:11 -0300

    new fonts and colors has been added

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default async function Home(){
+
+  return (
+    <main className="grid place-items-center h-screen">
+      <section>
+        <header>
+          <h1>BABY STORE</h1>
+        </header>
+      </section>
+    </main>
+  );
+}
