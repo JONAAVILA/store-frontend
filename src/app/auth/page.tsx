16461cbe9bccd4ab78fbb883d4bb45e5f9a1ea84
sidonay499 <jona_1969@hotmail.com>
2025-08-06 18:49:00 -0300

    login style and animation created

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,8 +2,7 @@ import LoginForm from "@/src/components/forms/loginForm";
 
 export default function Outh(){
     return(
-        <section>
-            <h1>login</h1>
+        <section className="grid h-[400px] items-center justify-center">
             <LoginForm/>
         </section>
     )
--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -2,21 +2,28 @@
 
 export default function LoginForm(){
     return(
-        <form action="" className="grid items-center w-[300px] text-left">
-            <label htmlFor="email">EMAIL</label>
-            <input
-                type="text" 
-                id="email"
-                name="email"
-                required
-            />
-            <label htmlFor="password">PASSWORD</label>
-            <input
-                type="text" 
-                id="password"
-                name="password"
-                required
-            />
+        <form action="" className="grid items-center w-[300px] text-left gap-4">
+            <h1>ingresar</h1>
+            <div className="relative">
+                <input
+                    className="peer"
+                    type="text" 
+                    id="email"
+                    name="email"
+                    required
+                />
+                <label className="absolute transition ease-in-out duration-300 left-2 top-2 text-[10px] peer-focus:-translate-y-6" htmlFor="email">EMAIL</label>
+            </div>
+            <div className="relative">
+                <input
+                    className="peer"
+                    type="text" 
+                    id="password"
+                    name="password"
+                    required
+                />
+                <label className="absolute transition ease-in-out duration-300 left-2 top-2 text-[10px] peer-focus:-translate-y-6" htmlFor="password">PASSWORD</label>
+            </div>
             <button>
                 INGRESAR
             </button>
